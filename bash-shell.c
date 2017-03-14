eeca6e10f6426ec402da433169bb0a5b2c444d1d
hinsonan <hinsonan@gmail.com>
2017-03-14 15:27:34 -0500

    forks the child

--- bash-shell.c
+++ bash-shell.c
@@ -1,5 +1,8 @@
+#include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
 
 
 #define MAX_LINE 80 /* The maximum length command */
@@ -11,10 +14,10 @@ int main(void){
     char *args[MAX_LINE/2 + 1]; /* command line arguments */
     int shouldRun = 1; /* flag to determine when to exit program */
 
-    
+
     char *argArray[3];
     //static input
-    char *cmd = "ls";
+
     argArray[0] = "ls";
     argArray[1] = "-la";
     argArray[2] = NULL;
@@ -23,8 +26,28 @@ int main(void){
         printf("osh>");
         fflush(stdout);
 
+        pid_t pid;
 
+        pid = fork();
 
+        if (pid < 0) {
+            fprintf(stderr, "Fork Failed");
+            return 1;
+        }
+        else if(pid == 0) {
+            execvp(argArray[0],  argArray);
+        }
+
+        else {
+          int i;
+            for (i = 0; i<2; i++)
+            {
+                if(strcmp(argArray[i],"&"))
+                {wait(NULL);
+                    }
+            }
+            printf("Child Complete\n");
+        }
 
 
         /**
@@ -33,9 +56,8 @@ int main(void){
          * (2) the child process will invoke execvp()
          * (3) if command included &, parent will invoke wait()
          */
-         execvp( argArray[0],  argArray);
         break;
-        }
+    }
 
     return 0;
 }
