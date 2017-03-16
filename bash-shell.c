427d896a24f9a86c10826d9ce8c71b1e56fd807b
hinsonan <hinsonan@gmail.com>
2017-03-16 14:45:42 -0500

    version 1.01

--- bash-shell.c
+++ bash-shell.c
@@ -4,60 +4,80 @@
 #include <sys/wait.h>
 #include <string.h>
 
+//declares a function that will parse the user input
+void parseArray (char cmdArray[], char *argArray[]);
 
 #define MAX_LINE 80 /* The maximum length command */
-
-
-
 
 int main(void){
     char *args[MAX_LINE/2 + 1]; /* command line arguments */
     int shouldRun = 1; /* flag to determine when to exit program */
 
 
-    char *argArray[3];
-    //static input
-
-    argArray[0] = "ls";
-    argArray[1] = "-la";
-    argArray[2] = NULL;
-
     while (shouldRun) {
+        fflush(stdout);
         printf("osh>");
         fflush(stdout);
 
+        //makes an array that holds the command
+        char cmdArray[50];
+        //makes an array that will hold the parsed values of the usr input
+        char *argArray[3];
+
+        //calls the parse array function
+        parseArray(cmdArray, argArray);
+
+        //initializes the pid
         pid_t pid;
 
         pid = fork();
+
+        //these blocks will execute based on the pid values
 
         if (pid < 0) {
             fprintf(stderr, "Fork Failed");
             return 1;
         }
         else if(pid == 0) {
+          //this line will execute the command the user inputed
             execvp(argArray[0],  argArray);
         }
 
         else {
-          int i;
-            for (i = 0; i<2; i++)
+            for (int i = 0; i<2; i++)
             {
-                if(strcmp(argArray[i],"&"))
+              //if there is a parent to wait on call the wait method
+                if( (strcmp(argArray[i],"&")) == '0')
                 {wait(NULL);
                     }
             }
             printf("Child Complete\n");
         }
 
-
-        /**
-         * After reading user input, the steps are:
-         * (1) fork a child process using fork()
-         * (2) the child process will invoke execvp()
-         * (3) if command included &, parent will invoke wait()
-         */
-        break;
+        shouldRun = 0;
     }
 
     return 0;
+}
+
+void parseArray (char cmdArray[], char *argArray[]){
+
+    //get the user input
+    fgets(cmdArray, 50, stdin);
+    //this char pointer will be used to help parse the command
+    char *command;
+    //these are the delimeters that will tell the strtok method when to parse the string
+    char spaces[]=" \t\r\n\v\f";
+    //parses the string when it gets to a delimiter
+    command = strtok(cmdArray, spaces);
+    int i = 0;
+
+    //this loop continues to parse the string until it reaches the end or gets to null
+    while( command != NULL )
+    {
+        argArray[i] = command;
+        i++;
+        command = strtok(NULL, spaces);
+    }
+
 }
